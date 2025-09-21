54863b1c411707d10e6d5d83042545e96e7d5653
caarloosdab <caarloosdab@gmail.com>
2025-09-20 22:36:55 -0600

    `Refactor database connection logic to use a single configuration object`

--- database/index.js
+++ database/index.js
@@ -6,22 +6,35 @@ require("dotenv").config()
  * But will cause problems in production environment
  * If - else will make determination which to use
  * *************** */
-let pool
+/*let pool
 if (process.env.NODE_ENV == "development") {
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
       rejectUnauthorized: false,
     },
-})
+})*/
 
 // Added for troubleshooting queries
 // during development
+const isDevelopment = process.env.NODE_ENV === "development"
+
+const poolConfig = {
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+}
+
+const pool = new Pool(poolConfig)
 module.exports = {
   async query(text, params) {
     try {
       const res = await pool.query(text, params)
-      console.log("executed query", { text })
+      //console.log("executed query", { text })
+       if (isDevelopment) {
+        console.log("executed query", { text })
+      }
       return res
     } catch (error) {
       console.error("error in query", { text })
@@ -29,9 +42,9 @@ module.exports = {
     }
   },
 }
-} else {
+/*} else {
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
   })
   module.exports = pool
-}
+}*/
