164af4d80c6647410f9209b98f5c04343f001b0e
caarloosdab <caarloosdab@gmail.com>
2025-09-20 23:34:33 -0600

    `Refactor server.js and utilities/index.js to add error handling and navigation functionality.`

--- server.js
+++ server.js
@@ -7,10 +7,11 @@
  *************************/
 const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
-const env = require("dotenv").config()
+require("dotenv").config()
 const app = express()
-const static = require("./routes/static")
+const staticRoutes = require("./routes/static")
 const baseController = require("./controllers/baseControllers")
+const util = require("./utilities") // <-- needed for util.getNav()
 
 /* ***********************
  * View Engine and Templates
@@ -18,28 +19,60 @@ const baseController = require("./controllers/baseControllers")
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
+
 /* ***********************
  * Routes
  *************************/
 const inventoryRoute = require("./routes/inventoryRoute")
-app.use(static)
+app.use(staticRoutes)
 
 // Index route
-app.get("/", baseController.buildHome)
+app.get("/", util.handleErrors(baseController.buildHome))
 
 // Inventory routes
 app.use("/inv", inventoryRoute)
 
 /* ***********************
+ * File Not Found Route - must be after all routes
+ *************************/
+app.use(async (req, res, next) => {
+  try {
+    const nav = await util.getNav()
+    res.status(404).render("errors/error", {
+      title: "404 - Not Found",
+      message: "Sorry, we appear to have lost that page.",
+      nav
+    })
+  } catch (e) {
+    next(e)
+  }
+})
+
+/* ***********************
+ * Express Error Handler
+ * Place after all other middleware
+ *************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  })
+})
+
+/* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT || 5500
+const host = process.env.HOST || "0.0.0.0"
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
-app.listen(port, () => {
+app.listen(port, host, () => {
   console.log(`app listening on ${host}:${port}`)
 })
